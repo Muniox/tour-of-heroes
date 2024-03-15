5c4325b71f08c5fca4013ceb944a418b90d7fa7e
Muniox <muniox20@gmail.com>
2024-03-15 15:51:31 +0100

    Display the message from HeroService

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
-import { HEROES } from '../mock-heroes';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.scss',
 })
-export class HeroesComponent {
-  heroes = HEROES;
+export class HeroesComponent implements OnInit, OnDestroy {
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+
+  // While you could call getHeroes() in the constructor, that's not the best practice.
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
+  }
+
+  ngOnDestroy(): void {
+    // TODO: make unsubscribe
+    throw new Error('Method not implemented.');
+  }
 
   onSelect(hero: Hero) {
     this.selectedHero = hero;
